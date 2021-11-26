81720682c06768e36c5b7f02ff38f8d402cf17d5
Ary G. Lins <ary.lins@loft.com.br>
2021-11-25 22:40:30 -0300

    refactoring validate cpf step 2

--- src/validate-cpf.js
+++ src/validate-cpf.js
@@ -14,9 +14,6 @@ exports.validate = function(str) {
         return false;
     }
     try{  
-        let     dg2;  
-        let     nDigResult;  
-        dg2 = 0;  
         let d1 = 0;  
         let d2 = 0;
         for (let nCount = 1; nCount < str.length -1; nCount++) {  
@@ -24,20 +21,20 @@ exports.validate = function(str) {
             d1 = d1 + ( 11 - nCount ) * digito;  
             d2 = d2 + ( 12 - nCount ) * digito;  
         };  
-        const resto1 = (d1 % 11);  
-        const dg1 = (resto1 < 2) ? 0 : 11 - resto1;  
-        d2 += 2 * dg1;  
-        const resto2 = (d2 % 11);  
-        if (resto2 < 2)  
-            dg2 = 0;  
-        else  
-            dg2 = 11 - resto2;  
-            let nDigVerific = str.substring(str.length-2, str.length);  
-        nDigResult = "" + dg1 + "" + dg2;  
+        const dg1 = calculateVerificationDigit(d1);  
+        d2 += 2 * dg1;
+        const dg2 = calculateVerificationDigit(d2);
+        let nDigVerific = str.substring(str.length-2, str.length);  
+        const nDigResult = "" + dg1 + "" + dg2;  
         return nDigVerific == nDigResult;
     }catch (e){  
         console.error("Erro !"+e);  
 
         return false;  
     }  
+}
+
+function calculateVerificationDigit(d) {
+    const resto = (d % 11);
+    return (resto < 2) ? 0 : 11 - resto;
 }
