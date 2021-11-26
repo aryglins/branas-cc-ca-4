e6da1a89e2279e8c20088a29ae8c5d0d5c494ffb
Ary G. Lins <ary.lins@loft.com.br>
2021-11-25 22:28:22 -0300

    refactoring algorithm step 1

--- src/validate-cpf.js
+++ src/validate-cpf.js
@@ -14,35 +14,24 @@ exports.validate = function(str) {
         return false;
     }
     try{  
-        let     d1, d2;  
-        let     dg1, dg2, rest;  
-        let     digito;  
-            let     nDigResult;  
-        d1 = d2 = 0;  
-        dg1 = dg2 = rest = 0;  
-            
+        let     dg2;  
+        let     nDigResult;  
+        dg2 = 0;  
+        let d1 = 0;  
+        let d2 = 0;
         for (let nCount = 1; nCount < str.length -1; nCount++) {  
-            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-            // 	return false;
-            // } else {
-
-                digito = parseInt(str.substring(nCount -1, nCount));  							
-                d1 = d1 + ( 11 - nCount ) * digito;  
-
-                d2 = d2 + ( 12 - nCount ) * digito;  
-            // }
+            const digito = parseInt(str.substring(nCount -1, nCount));  							
+            d1 = d1 + ( 11 - nCount ) * digito;  
+            d2 = d2 + ( 12 - nCount ) * digito;  
         };  
-            
-        rest = (d1 % 11);  
-
-        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
+        const resto1 = (d1 % 11);  
+        const dg1 = (resto1 < 2) ? 0 : 11 - resto1;  
         d2 += 2 * dg1;  
-        rest = (d2 % 11);  
-        if (rest < 2)  
+        const resto2 = (d2 % 11);  
+        if (resto2 < 2)  
             dg2 = 0;  
         else  
-            dg2 = 11 - rest;  
-
+            dg2 = 11 - resto2;  
             let nDigVerific = str.substring(str.length-2, str.length);  
         nDigResult = "" + dg1 + "" + dg2;  
         return nDigVerific == nDigResult;
