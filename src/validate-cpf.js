c0b05bb9e65dc4c489724448dc5d4c0c92af7503
Ary G. Lins <ary.lins@loft.com.br>
2021-11-29 17:32:14 -0300

    refactoring validate cpf step 6

--- src/validate-cpf.js
+++ src/validate-cpf.js
@@ -1,40 +1,25 @@
-exports.validate = function(cpf) {
-    const cpfStripped = removeNonDigitsAllowedChars(cpf);  
-    if (validateInputCpfFormat(cpfStripped)) {
+exports.validate_cpf = function(cpf) {
+    if (validateCpfInputFormat(cpf)) {
         return false;
     }
-    const expectedVerificationDigitOne = calculateVerificationDigitOne(cpfStripped);  
-    const expectedVerificationDigitTwo = calculateVerificationDigitTwo(cpfStripped, expectedVerificationDigitOne);
+    const cpfStripped = removeNonDigitsAllowedChars(cpf);
+    if(validateCpfDigits(cpfStripped)) {
+        return false;
+    }
+    const cpfWithoutVerificationDigits = cpfStripped.substring(0, cpfStripped.length - 2);
+    const expectedVerificationDigitOne = calculateVerificationDigitOne(cpfWithoutVerificationDigits);  
+    const expectedVerificationDigitTwo = calculateVerificationDigitTwo(cpfWithoutVerificationDigits, expectedVerificationDigitOne);
     const verificationDigitOne = parseInt(cpfStripped[cpfStripped.length-2]);
     const verificationDigitTwo = parseInt(cpfStripped[cpfStripped.length-1]);
     return verificationDigitOne === expectedVerificationDigitOne && verificationDigitTwo === expectedVerificationDigitTwo;
 }
 
-function validateInputCpfFormat(cpfStripped) {
-    return isNullOrUndefined(cpfStripped) || doesNotHaveCpfLength(cpfStripped) || isStrWithSameDigits(cpfStripped || !isOnlyDigitsString(cpfStripped));
+function validateCpfInputFormat(cpf) {
+    return isNullOrUndefined(cpf) || doesNotHaveCpfLength(cpf);
 }
 
-function calculateVerificationDigitOne(cpf) {
-    const cpfWithoutVerificationDigits = cpf.substring(0, cpf.length - 2);
-    const calc = Array.from(cpfWithoutVerificationDigits).map(char => parseInt(char)).reduce((acc, digit, i) => acc + (11 - (i + 1)) * digit, 0);
-    const remainder = (calc % 11);
-    return (remainder < 2) ? 0 : 11 - remainder;
-}
-
-function calculateVerificationDigitTwo(cpf, verificationDigitOne) {
-    const cpfWithoutVerificationDigits = cpf.substring(0, cpf.length - 2);
-    const firstCalc = Array.from(cpfWithoutVerificationDigits).map(char => parseInt(char)).reduce((acc, digit, i) => acc + (12 - (i + 1)) * digit, 0);
-    const secondCalc = firstCalc + 2 * verificationDigitOne;
-    const remainder = (secondCalc % 11);
-    return (remainder < 2) ? 0 : 11 - remainder;
-}
-
-function isOnlyDigitsString(cpfStripped) {
-    return cpfStripped.split("").every(c => parseInt(c) !== NaN);
-}
-
-function isStrWithSameDigits(cpf) {
-    return cpf.split("").every(c => c === cpf[0]);
+function validateCpfDigits(cpfStripped) {
+    return isStrWithSameDigits(cpfStripped) || !isOnlyDigitsString(cpfStripped);
 }
 
 function removeNonDigitsAllowedChars(cpf) {
@@ -45,8 +30,28 @@ function removeNonDigitsAllowedChars(cpf) {
         .replace(" ", "");
 }
 
+function calculateVerificationDigitOne(cpfWithoutVerificationDigits) {
+    const calc = Array.from(cpfWithoutVerificationDigits).map(char => parseInt(char)).reduce((acc, digit, i) => acc + (10 - i) * digit, 0);
+    const remainder = (calc % 11);
+    return (remainder < 2) ? 0 : 11 - remainder;
+}
+
+function calculateVerificationDigitTwo(cpfWithoutVerificationDigits, verificationDigitOne) {
+    const calc = Array.from(cpfWithoutVerificationDigits).map(char => parseInt(char)).concat(verificationDigitOne).reduce((acc, digit, i) => acc + (11 - i) * digit, 0);
+    const remainder = (calc % 11);
+    return (remainder < 2) ? 0 : 11 - remainder;
+}
+
+function isOnlyDigitsString(cpfStripped) {
+    return Array.from(cpfStripped).every(c => !isNaN(parseInt(c)));
+}
+
+function isStrWithSameDigits(cpf) {
+    return cpf.split("").every(c => c === cpf[0]);
+}
+
 function doesNotHaveCpfLength(cpf) {
-    return cpf.length != 11;
+    return cpf.length != 11 && cpf.length != 14;
 }
 
 function isNullOrUndefined(cpf) {
--- test/validate-cpf.test.js
+++ test/validate-cpf.test.js
@@ -2,24 +2,36 @@ const validate_cpf = require("../src/validate-cpf");
 
 test("Deve retornar retornar false para cpf inválido: 111.111.111-11:", function () {
 	const cpf = "111.111.111-11";
-	const result = validate_cpf.validate(cpf);
+	const result = validate_cpf.validate_cpf(cpf);
 	expect(result).toBeFalsy();	
 });
 
 test("Deve retornar retornar false para cpf inválido: 123.456.789-99:", function () {
 	const cpf = "123.456.789-99";
-	const result = validate_cpf.validate(cpf);
+	const result = validate_cpf.validate_cpf(cpf);
+	expect(result).toBeFalsy();	
+});
+
+test("Deve retornar retornar false para cpf inválido: 935.41a.347-80:", function () {
+	const cpf = "935.41a.347-80";
+	const result = validate_cpf.validate_cpf(cpf);
+	expect(result).toBeFalsy();	
+});
+
+test("Deve retornar retornar false para cpf inválido: null:", function () {
+	const cpf = null;
+	const result = validate_cpf.validate_cpf(cpf);
+	expect(result).toBeFalsy();	
+});
+
+test("Deve retornar retornar false para cpf inválido: undefined:", function () {
+	const cpf = undefined;
+	const result = validate_cpf.validate_cpf(cpf);
 	expect(result).toBeFalsy();	
 });
 
 test("Deve retornar retornar true para cpf válido: 935.411.347-80:", function () {
 	const cpf = "935.411.347-80";
-	const result = validate_cpf.validate(cpf);
+	const result = validate_cpf.validate_cpf(cpf);
 	expect(result).toBeTruthy();	
-});
-
-test("Deve retornar retornar false para cpf inválido: 935.41a.347-80:", function () {
-	const cpf = "935.41a.347-80";
-	const result = validate_cpf.validate(cpf);
-	expect(result).toBeFalsy();	
 });
